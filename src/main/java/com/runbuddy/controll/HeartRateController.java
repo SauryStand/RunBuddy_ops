9b16ba0d160986e094f73d1cc513a3fe59d21e3c
wohuodong <janfongz@126.com>
2017-02-16 00:44:14 +0800

    small src commit

--- src/main/java/com/runbuddy/controll/HeartRateController.java
+++ src/main/java/com/runbuddy/controll/HeartRateController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // @RestController
 // @RequestMapping("heartrate")
 @Controller
+@RequestMapping("heart")
 public class HeartRateController extends BaseController {
 
     /**
@@ -22,7 +23,7 @@ public class HeartRateController extends BaseController {
     protected Logger logger = LoggerFactory
             .getLogger(HeartRateController.class);
 
-    @RequestMapping(value = "/heart")
+    @RequestMapping(value = "/")
     @ResponseBody
     public String upLoadHeartRate(String json) {
         logger.debug("-->>拦截测试输出");
